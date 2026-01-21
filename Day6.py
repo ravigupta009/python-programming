9d01d095fd72d923b2a5f148be5e1b2b184c83c2
Ravi Gupta <ravi720904@gmail.com>
2026-01-22 01:44:52 +0530

    Add files via upload

---
+++ Day6.py
@@ -0,0 +1,8 @@
+# print(5+6)
+# print(15-6)
+# print(15*7)
+# print(15/6)
+# print(15//6)
+# print(15%7)
+# print(2**4)
+
---
+++ day7.py
@@ -0,0 +1,20 @@
+# a = int(input("Thw value of a is:"))
+# b = int(input("Thw value of b is:"))
+
+# add = a+b
+# sub = a-b
+# muti = a*b
+# div = a/b
+
+# print("addiation :",add)
+# print("subtraction :",sub)
+# print("multipli :",muti)
+# print("divide :",div)
+
+a = 50
+b = 3
+
+print("The value of", a, "+", 3 ,"is ",a + b)
+print("The value of", a, "-", 3 ,"is ",a - b)
+print("The value of", a, "*", 3 ,"is ",a * b)
+print("The value of", a, "/", 3 ,"is ",a / b)
