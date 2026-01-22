fc20eb4d811167f4ed0fae47b4e0592e2448f31f
Ravi Gupta <ravi720904@gmail.com>
2026-01-23 00:11:45 +0530

    Add files via upload

---
+++ Day10.py
@@ -0,0 +1,6 @@
+apple = '''he said,
+hi ravi
+hey i am good
+I want to eat an apple'''
+
+print(apple)
---
+++ day9.py
@@ -0,0 +1,9 @@
+# a = input("Enter your name :")
+# print(a)
+
+x = input("Enter first number :")
+y = input("Enter second number :")
+
+print(x+y)
+
+# print(int(x)+int(y))#slow the error
