ca478a66cee7f77b8aae2aaaf38c964f13fa4841
Ravi Gupta <ravi720904@gmail.com>
2026-01-27 22:34:37 +0530

    Add files via upload

---
+++ Day14.py
@@ -0,0 +1,19 @@
+# appleprice = 210
+# budget = 200
+ 
+# if (appleprice <= budget):
+#  print("Alexa, add 1 kg apples to the cart.")
+# else:
+#  print("Alexa , do not add apples to the cart.")
+
+
+num = int(input("Enter the value of num: "))
+
+if (num < 0):
+    print("number is negative.")
+elif (num == 0):
+    print("NUmber is zero.")
+elif (num == 999):
+    print("Number is special.")
+else:
+    print("NUmber is positive.")
---
+++ day13.py
@@ -0,0 +1,9 @@
+a = int(input("Enter your age :"))
+print("your age is :",a)
+
+if(a>18):
+    print("you can drive")
+    # print("yes")
+else:
+    print("you cannot drive")
+    # print("no")
